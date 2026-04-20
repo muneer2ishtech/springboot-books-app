e580ce60ab99eb9573bb43519b5ee282f21f30ad
Muneer Ahmed Syed <muneer@ishtech.fi>
2026-04-20 17:28:50 +0300

    dummy table for test

--- src/test/resources/db/postgres/create_table_dummy.sql
+++ src/test/resources/db/postgres/create_table_dummy.sql
@@ -3,3 +3,10 @@ CREATE TABLE bookapp_dev_schema.t_dummy (
   is_active   BOOLEAN       NOT NULL DEFAULT true,
   description TEXT              NULL
 );
+
+INSERT INTO bookapp_dev_schema.t_dummy(is_active, description) VALUES
+(true, 'Test row 1'),
+(true, 'Test row 2'),
+(true, 'Test row 3');
+
+GRANT SELECT, DELETE ON TABLE bookapp_dev_schema.t_dummy TO bookapp_dev_user;
